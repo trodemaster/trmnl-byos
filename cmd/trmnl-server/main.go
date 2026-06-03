a8d88772fe0e8fb3e77be1ac2ee85adb647cbb3b
Blake Garner <blake@netjibbing.com>
2026-06-02 22:12:49 -0700

    Fix .gitignore to not exclude cmd/trmnl-server source directory

---
+++ cmd/trmnl-server/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/trodemaster/trmnl-byos/internal/device"
+	"github.com/trodemaster/trmnl-byos/server"
+
+	_ "github.com/trodemaster/trmnl-byos/plugins/clock"
+	_ "github.com/trodemaster/trmnl-byos/plugins/dashboard"
+	_ "github.com/trodemaster/trmnl-byos/plugins/forecast"
+	_ "github.com/trodemaster/trmnl-byos/plugins/weather"
+)
+
+func main() {
+	port := env("PORT", "8080")
+	baseURL := env("BASE_URL", "http://localhost:"+port)
+	dataDir := env("DATA_DIR", "./data")
+	debug := os.Getenv("DEBUG") != ""
+
+	store, err := device.NewStore(dataDir)
+	if err != nil {
+		log.Fatalf("device store: %v", err)
+	}
+
+	handler := server.New(store, baseURL, debug)
+	if debug {
+		log.Printf("listening on :%s (base URL: %s) [debug]", port, baseURL)
+	} else {
+		log.Printf("listening on :%s (base URL: %s)", port, baseURL)
+	}
+	log.Fatal(http.ListenAndServe(":"+port, handler))
+}
+
+func env(key, fallback string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+	return fallback
+}
